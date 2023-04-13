142a4bbda2b32c6c56dcdec9335127a67ab58328
yx155 <102385954+yx155@users.noreply.github.com>
2023-04-13 00:17:03 -0500

    final fix

--- store/views/addProduct.py
+++ store/views/addProduct.py
@@ -13,45 +13,49 @@ class addProduct(View):
         else:
             return redirect('login')
 
+    @cache_control(no_cache=True, must_revalidate=True, no_store=True)
     def post(self, request):
-        postData = request.POST
-        name = postData.get ('name')
-        price = postData.get ('price')
-        category = postData.get ('category')
-        seller = Customer.objects.get(id= request.session.get('customer')).email
-        description = postData.get ('description')
-        quantity = postData.get('quantity')
-        image = postData.get('image')
-        # validation
-        value = {
-            'name': name,
-            'price': price,
-            'category': category,
-            'description': description,
-            'quantity': quantity,
-            'image': image,
-            'seller': seller
-        }
-        error_message = None
-
-        product = Products (name=name,
-                             price=price,
-                             category=Category.objects.get(name= category),
-                             description=description,
-                             quantity=quantity,
-                             image="uploads/products/"+image,
-                             seller=seller)
-        error_message = self.validateProduct (product)
-
-        if not error_message:
-            product.register ()
-            return redirect ('sellerMenu')
-        else:
-            data = {
-                'error': error_message,
-                'values': value
+        if (request.session.get('customer')):
+            postData = request.POST
+            name = postData.get ('name')
+            price = postData.get ('price')
+            category = postData.get ('category')
+            seller = Customer.objects.get(id= request.session.get('customer')).email
+            description = postData.get ('description')
+            quantity = postData.get('quantity')
+            image = postData.get('image')
+            # validation
+            value = {
+                'name': name,
+                'price': price,
+                'category': category,
+                'description': description,
+                'quantity': quantity,
+                'image': image,
+                'seller': seller
             }
-            return render (request, 'addProduct.html', data)
+            error_message = None
+
+            product = Products (name=name,
+                                price=price,
+                                category=Category.objects.get(name= category),
+                                description=description,
+                                quantity=quantity,
+                                image="uploads/products/"+image,
+                                seller=seller)
+            error_message = self.validateProduct (product)
+
+            if not error_message:
+                product.register ()
+                return redirect ('sellerMenu')
+            else:
+                data = {
+                    'error': error_message,
+                    'values': value
+                }
+                return render (request, 'addProduct.html', data)
+        else:
+            return redirect('login')
 
     def validateProduct(self, product):
         error_message = None
--- store/views/editProduct.py
+++ store/views/editProduct.py
@@ -15,35 +15,38 @@ class editProduct(View):
         else:
             return redirect('login')
     
-    
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def saveProduct(request):
-    postData = request.POST
-    product = Products.objects.get(id=postData.get('product'))
-    name = postData.get ('name')
-    price = postData.get ('price')
-    category = postData.get ('category')
-    description = postData.get ('description')
-    quantity = postData.get('quantity')
-    image = postData.get('image')
-    
-    error_message = None
-    if int(price) < 1:
-        error_message = "The price must be a positive number!"
-    elif int(quantity) < 0:
-        error_message = "The number of product in stock cannot be a negative number!"
+    if (request.session.get('customer')):
+        postData = request.POST
+        product = Products.objects.get(id=postData.get('product'))
+        name = postData.get ('name')
+        price = postData.get ('price')
+        category = postData.get ('category')
+        description = postData.get ('description')
+        quantity = postData.get('quantity')
+        image = postData.get('image')
         
-    if not error_message:
-        product.name = name
-        product.price = price
-        product.category = Category.objects.get(name= category)
-        product.description = description
-        product.quantity = quantity
-        if image != "":
-            product.image = "uploads/products/"+image
-        product.save()
-        return redirect ('sellerMenu')
+        error_message = None
+        if int(price) < 1:
+            error_message = "The price must be a positive number!"
+        elif int(quantity) < 0:
+            error_message = "The number of product in stock cannot be a negative number!"
+            
+        if not error_message:
+            product.name = name
+            product.price = price
+            product.category = Category.objects.get(name= category)
+            product.description = description
+            product.quantity = quantity
+            if image != "":
+                product.image = "uploads/products/"+image
+            product.save()
+            return redirect ('sellerMenu')
+        else:
+            return render (request, 'editProduct.html', {'error': error_message, 'product': product})
     else:
-        return render (request, 'editProduct.html', {'error': error_message, 'product': product})
+        return redirect('login')
 
 
 def delete(request):
